7e653f6d79385eb7be3db8a7d30d97fb2b5f2f39
harendersingh15 <harendersingh15@gmail.com>
2017-03-13 23:31:39 +0530

    convert the app.js file to app.ts file and targeted the code to es6

--- app.js
+++ app.js
@@ -1,53 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Module dependencies.
  */
-
-var express = require('express'),
-    routes = require('./routes'),
-    user = require('./routes/user'),
-    http = require('http'),
-    path = require('path'),
-    fs = require('fs');
-
+const express = require("express");
+const path = require("path");
+const body_parser_1 = require("body-parser");
+const logger = require("morgan");
+const errorHandler = require("errorhandler");
+const multipart = require("connect-multiparty");
+const methodOverride = require("method-override");
+const fs = require("fs");
+// var express = require('express'),
+//     routes = require('./routes'),
+//     user = require('./routes/user'),
+//     http = require('http'),
+//     path = require('path'),
+//     fs = require('fs');
 var app = express();
-
 var db;
-
 var cloudant;
-
 var fileToUpload;
-
 var dbCredentials = {
     dbName: 'my_sample_db'
 };
-
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var logger = require('morgan');
-var errorHandler = require('errorhandler');
-var multipart = require('connect-multiparty')
 var multipartMiddleware = multipart();
-
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
+app.use(body_parser_1.urlencoded({
     extended: true
 }));
-app.use(bodyParser.json());
+app.use(body_parser_1.json());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/client', express.static(path.join(__dirname, '/client/app')));
 app.use('/style', express.static(path.join(__dirname, '/views/style')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-
 // development only
 if ('development' == app.get('env')) {
-    console.log('in to the development area')
-        //app.use(express.static(path.join(__dirname, 'node_modules')));
+    console.log('in to the development area');
+    //app.use(express.static(path.join(__dirname, 'node_modules')));
     app.use(errorHandler());
 }
 
@@ -68,8 +64,7 @@ function initDBConnection() {
     //containing all the service credentials of all the bound services
     if (process.env.VCAP_SERVICES) {
         dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
-    } else { //When running locally, the VCAP_SERVICES will not be set
-
+    } else {
         // When running this app locally you can get your Cloudant credentials
         // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
         // Variables section for an app in the Bluemix console dashboard).
@@ -79,25 +74,18 @@ function initDBConnection() {
         // url will be in this format: https://username:password@xxxxxxxxx-bluemix.cloudant.com
         dbCredentials.url = getDBCredentialsUrl(fs.readFileSync("vcap-local.json", "utf-8"));
     }
-
     cloudant = require('cloudant')(dbCredentials.url);
-
     // check if DB exists if not create
     cloudant.db.create(dbCredentials.dbName, function(err, res) {
         if (err) {
             console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
         }
     });
-
     db = cloudant.use(dbCredentials.dbName);
 }
-
 initDBConnection();
-
 // app.get('/', routes.index);
-
 app.get('/test', function(request, response) {
-
     var rowData = [];
     db.list({ include_docs: true }, function(err, result) {
         if (err) {
@@ -109,9 +97,8 @@ app.get('/test', function(request, response) {
             });
             response.send(rowData);
         }
-    })
+    });
 });
-
 app.get('/api/employee', function(request, response) {
     var rowData = [];
     db.list({ include_docs: true }, function(err, result) {
@@ -124,9 +111,8 @@ app.get('/api/employee', function(request, response) {
             });
             response.send(rowData);
         }
-    })
+    });
 });
-
 app.get('/api/employee/employee/:id', function(req, res) {
     var id = req.params.id;
     console.log(id);
@@ -139,18 +125,15 @@ app.get('/api/employee/employee/:id', function(req, res) {
             res.status(404).send('Doc not found');
         }
     });
-
 });
-
 app.post('/api/employee/employee', function(request, response) {
-    console.log('hello new emp')
+    console.log('hello new emp');
     var data = request.body.data || request.body;
     var emp = {
         'name': data.name,
         'age': data.age,
         'mobile': data.mobile
     };
-
     db.insert(emp, function(err, doc) {
         if (err) {
             console.log(err);
@@ -166,11 +149,8 @@ app.post('/api/employee/employee', function(request, response) {
         }
     });
 });
-
 app.delete('/api/employee/employee/:id', function(request, response) {
-
     var id = request.params.id;
-
     db.get(id, {
         revs_info: true
     }, function(err, doc) {
@@ -187,9 +167,10 @@ app.delete('/api/employee/employee/:id', function(request, response) {
         }
     });
 });
-
-
-
-http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
-    console.log('Express server listening on port ' + app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log('server is running on port number' + app.get('port'));
 });
+// http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
+//     console.log('Express server listening on port ' + app.get('port'));
+// }); 
+//# sourceMappingURL=app.js.map
